3f36c836c2fb79da0e51f9dea980c33c4636f557
Will Busby <will0609@hotmail.com>
2018-07-29 23:23:45 +0100

    Version 0.0.4 - Better exporing (tweaked)

--- react-bem-components.js
+++ react-bem-components.js
@@ -1,6 +1,6 @@
 
-import {default as Wrapper} from "./components/Wrapper";
-import {default as Title} from "./components/Title";
-import {default as Text} from "./components/Text";
-import {default as Button} from './components/Button';
-import {default as Link} from './components/Link';
+export {default as Wrapper} from "./components/Wrapper";
+export {default as Title} from "./components/Title";
+export {default as Text} from "./components/Text";
+export {default as Button} from './components/Button';
+export {default as Link} from './components/Link';

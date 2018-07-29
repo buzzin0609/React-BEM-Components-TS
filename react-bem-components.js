6d0a5cd254ef79f3986c8bcbaede6ccd0cf30627
Will Busby <will0609@hotmail.com>
2018-07-29 23:09:39 +0100

    Added parcel build

--- react-bem-components.js
+++ react-bem-components.js
@@ -1,11 +1,13 @@
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import Text from "./components/Text";
-import Button from "./components/Button";
+import Button from './components/Button';
+import Link from './components/Link';
 
 export default {
     Wrapper,
     Title,
     Text,
-    Button
+    Button,
+    Link
 };

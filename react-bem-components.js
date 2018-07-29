8de436fb45cd77fbabffc2bfebde041fd0dc040a
Will Busby <will0609@hotmail.com>
2018-07-29 23:22:57 +0100

    Version 0.0.4 - Better exporting

--- react-bem-components.js
+++ react-bem-components.js
@@ -1,13 +1,6 @@
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import Text from "./components/Text";
-import Button from './components/Button';
-import Link from './components/Link';
 
-export default {
-    Wrapper,
-    Title,
-    Text,
-    Button,
-    Link
-};
+import {default as Wrapper} from "./components/Wrapper";
+import {default as Title} from "./components/Title";
+import {default as Text} from "./components/Text";
+import {default as Button} from './components/Button';
+import {default as Link} from './components/Link';

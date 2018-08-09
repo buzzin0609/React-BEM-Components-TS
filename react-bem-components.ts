c4d8a30c91df79951b4c6d6846f12a51a254aa18
william.busby <busbybmw69>
2018-08-09 15:59:32 +0100

    Version 0.1.0 - Changed exports back

--- react-bem-components.ts
+++ react-bem-components.ts
@@ -1,17 +1,8 @@
-import Wrapper from './components/Wrapper';
-import Title from './components/Title';
-import Text from './components/Text';
-import Button from './components/Button';
-import Link from './components/Link';
-import Image from './components/Image';
-import Picture from './components/Picture';
 
-export default {
-    Wrapper,
-    Title,
-    Text,
-    Button,
-    Link,
-    Image,
-    Picture
-};
+export {default as Wrapper} from "./components/Wrapper";
+export {default as Title} from "./components/Title";
+export {default as Text} from "./components/Text";
+export {default as Button} from './components/Button';
+export {default as Link} from './components/Link';
+export {default as Image} from './components/Image';
+export {default as Picture} from './components/Picture';
